4248700a6d832d5430cfd4a8893b6b0f5a5e55f3
zhaoshubin <zhaoshubin@bilibili.com>
2021-08-04 18:58:54 +0800

    0804_1

--- cmd/main.py
+++ cmd/main.py
@@ -2,6 +2,7 @@ from flask_apscheduler import APScheduler
 from internal.job.chaos import chaos_func
 from internal.job.crawler_xiachufang import get_foods_job
 from internal.server.main import app
+from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
 import fcntl
 from internal.utils.log import LOGGER
@@ -9,16 +10,18 @@ from internal.utils.log import LOGGER
 
 def start_server():
     f = open("scheduler.lock", "wb")
+
     try:
+
         fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        scheduler = APScheduler()
-        scheduler.init_app(app)
+        scheduler = BackgroundScheduler()
+
+        scheduler.add_job(func=chaos_func, id='apscheduler_chaos', trigger='interval', minutes=30,
+                          replace_existing=True)
+        # scheduler.add_job(func=get_foods_job, id='apscheduler_xcf', trigger='interval', minutes=0.1,
+        #                   replace_existing=True)
         scheduler.start()
-        scheduler.add_job(func=chaos_func, id='apscheduler_chaos', args=None, trigger='interval', minutes=30,
-                          replace_existing=True)
-        scheduler.add_job(func=get_foods_job, id='apscheduler_xcf', args=None, trigger='interval', minutes=120,
-                          replace_existing=True)
     except Exception as e:
         LOGGER.warning("Locked on scheduler.lock")
-
     app.run(host="0.0.0.0", port=10009, debug=True)
+
--- internal/job/crawler_xiachufang.py
+++ internal/job/crawler_xiachufang.py
@@ -1,34 +1,40 @@
 import httpx
+import requests
 from bs4 import BeautifulSoup
 from internal.utils.json_reader import json_writer, json_reader
+import logging
+
 
 def __get_foods_single_page(number: int):
     url = "https://www.xiachufang.com/category/40076/?page={}".format(str(number))
     headers = {
-        'content-type': 'text/html'
+        'content-type': 'text/html',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+        "Host": "www.xiachufang.com"
     }
     res_list = []
-    try:
-        with httpx.Client() as cli:
-            resp = cli.get(url=url, headers=headers)
-        page = resp
-        content = BeautifulSoup(page.content, 'html.parser')
-        content = content.find_all(class_="recipe")
-        for item in content:
-            rep_dict = {}
-            item_content = item.find(class_="name")
-            rep_dict["name"] = item_content.text.replace("\n", "").replace(" ", "")
-            rep_dict["link"] = "https://xiachufang.com" + item_content.find('a')['href']
-            try:
-                img = item.find('img')['data-src']
-                rep_dict['img'] = img
-            except:
-                img = item.find('img')['src']
-                rep_dict['img'] = img
-            res_list.append(rep_dict)
-    except:
-        return
-
+    resp = requests.get(url=url, headers=headers)
+    # with httpx.Client() as cli:
+    #     resp = cli.get(url=url, headers=headers)
+    logging.info(resp.status_code)
+    page = resp
+    content = BeautifulSoup(page.content, 'html.parser', from_encoding="utf-8")
+    assert content
+    content = content.find_all(class_="recipe")
+    assert content
+    for item in content:
+        rep_dict = {}
+        item_content = item.find(class_="name")
+        logging.info(str(item_content))
+        rep_dict["name"] = item_content.text.replace("\n", "").replace(" ", "")
+        rep_dict["link"] = "https://xiachufang.com" + item_content.find('a')['href']
+        try:
+            img = item.find('img')['data-src']
+            rep_dict['img'] = img
+        except:
+            img = item.find('img')['src']
+            rep_dict['img'] = img
+        res_list.append(rep_dict)
     return res_list
 
 
@@ -38,7 +44,7 @@ def get_foods_job():
     :return:
     """
     res_list = []
-    for i in range(1, 10):
+    for i in range(1, 8):
         fs = __get_foods_single_page(i)
         res_list.extend(fs)
     json_writer("static/boot_food/xiachufang.json", res_list)
